7a7774cda79d0c9911603432c4d6046bcfa0f5bc
cesarniebla <cesarnniebla@gmail.com>
2018-12-04 19:10:52 -0600

    ciclos.js

---
+++ html/ciclos.js
@@ -0,0 +1,39 @@
+
+var elBody=document.getElementsByTagName('body') [0];
+
+for (i= 0; i<=100; i++) {
+    var elBoton=document.createElement('button');
+    elBoton.textContent="texto para boton";
+    elBody.appendChild(elBoton);
+}
+
+
+
+/**
+for(conta = 0; conta < 10000; conta++) {
+    console.log('Me canso ganso'+conta);
+    if (conta==7500)
+        break;
+
+        for(conta = 2; conta <= 10; conta++) {
+    console.log ('2x'+conta+' = '+(conta*2))
+}
+}
+
+for (j=1; j <= 10; j++) {
+    console.log('Tabla del' ${]'
+    for(i = 1; i <= 10; i++) {
+        console.log ('1x'+i+' = '+(i*1));
+    }
+    
+}
+
+HACER 100 BOTONES
+
+for (i= 0; i<=100; i++) {
+    var elBoton=document.createElement('button');
+    elBoton.textContent="texto para boton";
+    var elBody=document.getElementsByTagName('body') [0];
+    elBody.appendChild(elBoton);
+
+ */
