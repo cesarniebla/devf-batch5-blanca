54e7c31e64226707f63c1582d1902051e36a07a5
cesarniebla <cesarnniebla@gmail.com>
2018-12-04 19:19:52 -0600

    clase-js.js

---
+++ html/clase-js.js
@@ -0,0 +1,60 @@
+/* console.log('ya cargo el js');
+
+console.error('ya valio'); comentario en mas de 1 linea
+*/
+
+// comentario en una sola linea
+
+
+var edad=65;
+
+
+if(edad>=18) {
+    console.log('es mayor de 18');
+
+    if(edad>=18 & edad<= 24) {
+        console.log('universidad');
+    }else{
+        if(edad>=25 & edad<= 28) {
+            console.log('Posgrado');
+        }else{
+            if(edad>=29 & edad<= 33) {
+                console.log('adulto joven');
+            }else{
+                if(edad>=34 & edad<= 40) {
+                    console.log('chavorruco');
+                }else{
+                    if(edad>=41 & edad<= 47) {
+                        console.log('señor crisis');
+                    }else{
+                        if(edad>=48 & edad<= 60) {
+                            console.log('viejo lesbiano');
+                        }else{
+                            if(edad>=61 & edad<= 99) {
+                                console.log('suggar daddy');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}else{
+    console.log('es menor de 18');
+
+    if(edad>=6 & edad<=12) {
+        console.log('a primaria debes de ingresar');
+    }else{
+        if(edad>=13 & edad<=15) {
+            console.log('secundaria');
+        }else{
+            if (edad>=16 & edad<=17) {
+            console.log('prepa');
+            }
+        }
+    
+    }
+    
+
+}
